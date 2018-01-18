71bf1a798364d1c796eb0f09a76dee64329757a7
aliounemane2 <MANE@mbp-de-nema.home>
2018-01-18 10:45:37 +0000

    FRONT END LIV’IN DKR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { UserService } from './shared_service/user.service';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { InstitutionComponent } from './institution/institution.component';
+import { EvenementComponent } from './evenement/evenement.component';
+import { User } from 'app/classes/user';
+import { EvenementService } from 'app/shared_service/evenement.service';
+import { ListesInstitutionsComponent } from './listes-institutions/listes-institutions.component';
+import { ListeEvenementsComponent } from './liste-evenements/liste-evenements.component';
+import { DeleteInstitutionComponent } from './delete-institution/delete-institution.component';
+import { DeleteEventComponent } from './delete-event/delete-event.component';
+
+export const appRoutes:Routes=[
+  {path: '', redirectTo: '/institution',pathMatch: 'full'},
+  {path:'institution', component:InstitutionComponent },
+  {path:'evenement', component:EvenementComponent },
+  {path:'listeInstitution', component:ListesInstitutionsComponent },
+  {path:'listeEvent', component:ListeEvenementsComponent },
+  {path:'deleteEvent/:id', component:DeleteEventComponent },
+  {path:'deleteInstitution/:id', component:DeleteInstitutionComponent },
+
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InstitutionComponent,
+    EvenementComponent,
+    ListesInstitutionsComponent,
+    ListeEvenementsComponent,
+    DeleteInstitutionComponent,
+    DeleteEventComponent,
+    // User
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    ReactiveFormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  exports: [ RouterModule ],
+  providers: [UserService, EvenementService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
